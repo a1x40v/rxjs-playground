7c4c4afee57afb9602762f0a25ba1a68c6a69d34
a1x40v <a1x40v@gmail.com>
2024-08-14 18:14:17 +0300

    Create manual interval observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,38 @@ import { LoginComponent } from './login/login.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-playground';
+
+  ngOnInit(): void {
+    const observable$ = new Observable<number>((subscriber) => {
+      let count = 0;
+
+      /* sync value */
+      subscriber.next(count++);
+
+      const intervalId = setInterval(() => {
+        /* async value */
+        subscriber.next(count++);
+      }, 1000);
+
+      return () => {
+        console.log('clean up');
+        clearInterval(intervalId);
+      };
+    });
+
+    const observer: Partial<Observer<number>> = {
+      next: (value: number) => {
+        console.log(value);
+      },
+      complete: () => console.log('Complete!'),
+    };
+
+    const sub = observable$.subscribe(observer);
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    }, 3500);
+  }
 }
